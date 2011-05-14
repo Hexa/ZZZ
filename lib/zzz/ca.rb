196937885026b8cc36a6d51fa59af1026f55d327
Hexa <hexa.diary@gmail.com>
2011-05-15 00:02:56 +0900

    ca.rb を zzz 以下へ移動させた

---
+++ lib/zzz/ca.rb
@@ -0,0 +1,14 @@
+#!/opt/local/bin/ruby1.9
+# -*- coding: utf-8 -*-
+
+require File.join(File.expand_path(File.dirname(__FILE__)), "ca", "utils")
+require File.join(File.expand_path(File.dirname(__FILE__)), "ca", "x509")
+require File.join(File.expand_path(File.dirname(__FILE__)), "ca", "certificate")
+require File.join(File.expand_path(File.dirname(__FILE__)), "ca", "request")
+require File.join(File.expand_path(File.dirname(__FILE__)), "ca", "crl")
+require File.join(File.expand_path(File.dirname(__FILE__)), "ca", "subject_encoder")
+require File.join(File.expand_path(File.dirname(__FILE__)), "ca", "extension_encoder")
+
+
+module CA
+end
